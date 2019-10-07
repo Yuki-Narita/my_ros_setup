07351e90591642fbc137676f96ddfc8b78a69cc6
Yuki-Narita <18kmj27@ms.dendai.ac.jp>
2019-10-07 23:14:25 +0900

    Update ros_setup.sh

--- ros_setup.sh
+++ ros_setup.sh
@@ -1,19 +1,18 @@
-#!/usr/bin/env sh
+#!/bin/bash
 
 ###########################################
 # ROS環境セットアップ用シェルスクリプト(kinetic) #
 ###########################################
 
-printf "password: "
-read password
+read -p "password? : " password
 
 #######################
 # システムのアップデート #
 #######################
 
-echo "$password" | sudo -S apt -y update
-echo "$password" | sudo -S apt -y upgrade
-echo "$password" | sudo -S apt -y dist-upgrade
+echo $password | sudo -S apt -y update
+echo $password | sudo -S apt -y upgrade
+echo $password | sudo -S apt -y dist-upgrade
 
 ########################
 # home 以下を英語に変更 #
@@ -26,7 +25,7 @@ env LANG=C xdg-user-dirs-gtk-update
 # デュアルブートでの時計ズレを修正 #
 ################################
 
-echo "$password" | sudo -S timedatectl set-local-rtc true
+echo $password | sudo -S timedatectl set-local-rtc true
 
 
 #################
@@ -54,22 +53,22 @@ echo "alias terminatorm='terminator -l rosmulti & exit'" >> ~/.bashrc
 # ROSのインストール #
 ###################
 
-echo "$password" | sudo -S sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+echo $password | sudo -S sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 yes | sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
-echo "$password" | sudo -S apt -y update
-echo "$password" | sudo -S apt -y install ros-kinetic-desktop-full
-echo "$password" | sudo -S rosdep init
+echo $password | sudo -S apt -y update
+echo $password | sudo -S apt -y install ros-kinetic-desktop-full
+echo $password | sudo -S rosdep init
 rosdep update
 echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
 source ~/.bashrc
-echo "$password" | sudo -S apt -y install python-rosinstall
+echo $password | sudo -S apt -y install python-rosinstall
 
 
 ########################
 # ワークスペースの設定 #
 ########################
 
-echo "$password" | sudo -S apt -y install python-catkin-tools
+echo $password | sudo -S apt -y install python-catkin-tools
 mkdir -p ~/catkin_ws/src
 cd ~/catkin_ws && catkin build
 cd ~
@@ -80,20 +79,20 @@ source ~/.bashrc
 # turtlebot関連の設定 #
 #######################
 
-echo "$password" | sudo -S apt -y install ros-kinetic-turtlebot ros-kinetic-turtlebot-apps ros-kinetic-turtlebot-interactions ros-kinetic-turtlebot-simulator ros-kinetic-kobuki-ftdi ros-kinetic-ar-track-alvar-msgs
+echo $password | sudo -S apt -y install ros-kinetic-turtlebot ros-kinetic-turtlebot-apps ros-kinetic-turtlebot-interactions ros-kinetic-turtlebot-simulator ros-kinetic-kobuki-ftdi ros-kinetic-ar-track-alvar-msgs
 . /opt/ros/kinetic/setup.bash
 rosrun kobuki_ftdi create_udev_rules
 echo "export TURTLEBOT_3D_SENSOR=kinect" >> ~/.bashrc
 source ~/.bashrc
-echo "$password" | sudo -S apt -y install ros-kinetic-rtabmap-ros
+echo $password | sudo -S apt -y install ros-kinetic-rtabmap-ros
 
 ##########################
 # その他ツールのインストール #
 ##########################
-echo "$password" | sudo -S add-apt-repository -y ppa:maarten-baert/simplescreenrecorder
-echo "$password" | sudo -S apt -y update
-echo "$password" | sudo -S apt -y install simplescreenrecorder
-echo "$password" | sudo -S apt install -y vim terminator openssh-server
+echo $password | sudo -S add-apt-repository -y ppa:maarten-baert/simplescreenrecorder
+echo $password | sudo -S apt -y update
+echo $password | sudo -S apt -y install simplescreenrecorder
+echo $password | sudo -S apt install -y vim terminator openssh-server
 
 # terminator設定
 mkdir -p ~/.config/terminator
@@ -117,10 +116,10 @@ catkin build
 # 後片付け #
 ##########
 
-echo "$password" | sudo -S apt -y update
-echo "$password" | sudo -S apt -y upgrade
-echo "$password" | sudo -S apt -y dist-upgrade
-echo "$password" | sudo -S apt -y autoremove
+echo $password | sudo -S apt -y update
+echo $password | sudo -S apt -y upgrade
+echo $password | sudo -S apt -y dist-upgrade
+echo $password | sudo -S apt -y autoremove
 
 echo スクリプトが終了しました
 echo あとはVScodeとchromeの最新版をインストールしてください
