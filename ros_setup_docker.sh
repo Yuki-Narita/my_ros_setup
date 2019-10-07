fe1d7db56e867a9a319eb60f02d03087e1bb3ed5
Yuki-Narita <18kmj27@ms.dendai.ac.jp>
2019-10-07 20:07:57 +0900

    Update ros_setup_docker.sh

--- ros_setup_docker.sh
+++ ros_setup_docker.sh
@@ -51,7 +51,7 @@ echo "alias terminatorm='terminator -l rosmulti & exit'" >> ~/.bashrc
 # ROSのインストール #
 ###################
 
-sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+sh -c 'echo "deb http://packages.ros.org/ros/ubuntu kinetic main" > /etc/apt/sources.list.d/ros-latest.list'
 yes |  apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 apt -y update
 apt -y install ros-kinetic-desktop-full
